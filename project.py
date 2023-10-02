ce9639898a813e7eeb102992c85476dba0b68fe2
Isabelle Sharp <144169089+imsharp2024@users.noreply.github.com>
2023-10-01 19:51:43 -0500

    Update project.py

--- project.py
+++ project.py
@@ -3,6 +3,8 @@ import pygame
 pygame.init()
 x = 1000
 y = 700
+startTime = pygame.time.get_ticks()
+currentTime = 0
 
 screen = pygame.display.set_mode((x, y))
 
@@ -11,7 +13,10 @@ pygame.display.set_caption('laser tag')
 image = pygame.image.load("/Users/megan/Desktop/Software Engineering/PROJECT/splashscreen Large.jpg").convert()
 image_size = (1000, 700)
 image = pygame.transform.scale(image, image_size)
-screen.blit(image, (0,0))
+currentTime = (pg.time.get_ticks()-click_time) / 1000
+while currentTime >= 3:
+    screen.blit(image, (0,0))
+    currentTime = (pg.time.get_ticks()-click_time) / 1000
 
 # screen.fill([125,255,255])
 # screen.blit(screen, (1000, 700))
